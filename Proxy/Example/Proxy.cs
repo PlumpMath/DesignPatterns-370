23fbc52610212a56dc9bac8f37f3f7c9608a8771
lu <1686143231@qq.com>
2016-11-04 22:37:23 +0800

    Proxy

---
+++ Proxy/Example/Proxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Proxy : IGiveGift
+    {
+        private Pursuit gg;
+        public Proxy(SchoolGirl mm)
+        {
+            this.gg = new Pursuit(mm);
+        }
+
+        public void GiveDolls()
+        {
+            gg.GiveDolls();
+        }
+
+        public void GiveFlowers()
+        {
+            gg.GiveFlowers();
+        }
+
+        public void GiveChocolate()
+        {
+            gg.GiveChocolate();
+        }
+    }
+}
---
+++ Proxy/Example/SchoolGirl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example
+{
+    class SchoolGirl
+    {
+        public string Name { get; set; }
+    }
+}
